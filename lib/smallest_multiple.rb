ce756e0bb67a85825cd92e945c6e75195c9dce32
Jesse Smith <jesse.lee.smith@gmail.com>
2021-09-08 20:02:02 -0700

    solution

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,68 @@
 # Implement your procedural solution here!
+def smallest_multiple(divisorMax)
+    factorArray = collectPrimeFactors(divisorMax)
+
+    primeFactorObject = combinePrimeFactors(factorArray)
+
+    total = 1
+
+    primeFactorObject.each do |key, value|
+        total *= key.to_i ** value
+    end
+    total
+end
+
+def primeFactors(number)
+    factors = {}
+
+    while number % 2 == 0
+        if factors[2].nil? 
+            factors[2] = 1
+        else
+            factors[2] += 1
+        end
+        number /= 2
+    end
+
+    i = 3
+
+    while i <= Math.sqrt(number)
+        while number % i == 0
+            if factors[i].nil? 
+                factors[i] = 1
+            else
+                factors[i] += 1
+            end
+            number /= i
+        end
+
+        i +=  2
+    end
+
+    if( number > 2)
+        factors[number] = 1
+    end
+
+    factors
+end
+
+def collectPrimeFactors(divisorMax)
+    (2..divisorMax).collect do |number| 
+        primeFactors(number)
+    end
+end
+
+
+def combinePrimeFactors(arrayOfPrimeFactors)
+    combinedPrimeFactors = {}
+
+    arrayOfPrimeFactors.each do |factors|
+        factors.each do |key, value|
+            if combinedPrimeFactors[key].nil? || value > combinedPrimeFactors[key]
+                combinedPrimeFactors[key] = value
+            end
+        end
+    end
+
+    combinedPrimeFactors
+end
